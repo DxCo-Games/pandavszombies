c426f8234ad32c0469432f903a5d172f94c7547d
gsosarolon <gonzalososarolon@gmail.com>
2014-05-12 21:41:10 -0300

    minimos cambios

--- Classes/crimson/Player.h
+++ Classes/crimson/Player.h
@@ -8,12 +8,17 @@ namespace dxco {
 
 class GameModel;
 
-class Player :public Item {
+class Player : public Item {
 public:
 	Player(cocos2d::CCSprite* sprite, std::map<int, Animation*>& animations);
 	void disparar();
 
 	GameModel* model;
+	enum estados { CAMINANDO, QUIETO };
+
+	int getState();
+
+	int state;
 };
 
 } /* namespace dxco */
--- Classes/dxco/Joystick.h
+++ Classes/dxco/Joystick.h
@@ -8,8 +8,8 @@ class Joystick {
 
 public:
 	Joystick(cocos2d::CCSprite* sprite, float radio);
-	virtual void onTouchedBegan(cocos2d::CCPoint location, float angle, float intensity);
 	virtual void onMoved(cocos2d::CCPoint location, float angle, float intensity);
+	virtual void onTouchedBegan(cocos2d::CCPoint location, float angle, float intensity);
 	virtual void onTouchedEnded(cocos2d::CCPoint location, float angle, float intensity);
 	cocos2d::CCPoint getCenter();
 	float getRadio();
