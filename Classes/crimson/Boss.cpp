7a5bab0a0ba68a6a05aae39ba798ab2a39abb46d
Facundo Olano <facundo.olano@gmail.com>
2014-05-15 12:50:24 -0300

    added Boss class (not used yet)

---
+++ Classes/crimson/Boss.cpp
@@ -0,0 +1,19 @@
+/*
+ * Boss.cpp
+ *
+ *  Created on: May 15, 2014
+ *      Author: facundo
+ */
+
+#include "Boss.h"
+
+namespace dxco {
+
+Boss::Boss(GameModel* model, cocos2d::CCSprite* sprite, std::map<int, Animation*>& animations) :
+	Enemy(model, sprite, animations){
+	this-> life = 120;
+	this->deadTime = 0;
+}
+
+
+} /* namespace dxco */
