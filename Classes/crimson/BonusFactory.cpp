e2f91df0d3ed5eee8bf0e1e73c678a62d745483f
Facundo Olano <facundo.olano@gmail.com>
2014-05-20 21:11:47 -0300

    added pistol bonus

--- Classes/crimson/BonusFactory.cpp
+++ Classes/crimson/BonusFactory.cpp
@@ -15,7 +15,7 @@ void BonusFactory::createBonus(GameModel* model, cocos2d::CCPoint location) {
 	if (rand() % 100 < BONUS_PROBABILITY) {
 		std::map<int, dxco::Animation*> animations;
 		Bonus* bonus;
-		switch (rand() % 3) {
+		switch (rand() % 4) {
 			case 0: {	//Health bonus
 				cocos2d::CCSprite* bonusSprite = dxco::SpriteUtil::create("health.png", location.x, location.y, 30, 20);
 				bonus = (Bonus*) new HealthBonus(model, bonusSprite, animations);
@@ -29,6 +29,11 @@ void BonusFactory::createBonus(GameModel* model, cocos2d::CCPoint location) {
 			case 2: {
 				cocos2d::CCSprite* bonusSprite = dxco::SpriteUtil::create("smg.png", location.x, location.y, 40, 30);
 				bonus = (Bonus*) new WeaponBonus(model, bonusSprite, animations, Player::SMG_);
+				break;
+			}
+			case 3: {
+				cocos2d::CCSprite* bonusSprite = dxco::SpriteUtil::create("p.png", location.x, location.y, 35, 30);
+				bonus = (Bonus*) new WeaponBonus(model, bonusSprite, animations, Player::PISTOL);
 				break;
 			}
 		}
