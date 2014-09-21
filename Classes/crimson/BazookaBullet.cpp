b588facf6f6b8a28235ef7f532ee9ab772e6e236
gsosarolon <gonzalososarolon@gmail.com>
2014-09-21 15:45:08 -0300

    agrego lo inagregable

--- Classes/crimson/BazookaBullet.cpp
+++ Classes/crimson/BazookaBullet.cpp
@@ -6,21 +6,41 @@
  */
 
 #include "BazookaBullet.h"
+#include "GameModel.h"
+#include "cocos2d.h"
+#include "../dxco/MathUtil.h"
+#include "HelloWorldScene.h"
 
 namespace dxco {
 
-BazookaBullet::BazookaBullet(cocos2d::CCSprite* sprite, float angle, std::map<int, Animation*>& animations) : Bullet(sprite, angle, animations) {
-
+BazookaBullet::BazookaBullet(GameModel* model, cocos2d::CCSprite* sprite, float angle, std::map<int, Animation*>& animations) : Bullet(sprite, angle, animations) {
+	this->model = model;
 }
 
 void BazookaBullet::update(float dt) {
 	if (!this->used) {
 		Item::update(dt);
-		this->move(cos(this->angle) * BULLET_SPEED / 4 * dt, sin(this->angle) * BULLET_SPEED / 4 * dt);
+		this->move(cos(this->angle) * BULLET_SPEED * dt, sin(this->angle) * BULLET_SPEED * dt);
 	}
 }
 
+void BazookaBullet::use() {
+	cocos2d::CCParticleSystemQuad* firework = cocos2d::CCParticleSystemQuad::create("bazooka_ball.plist");
 
+	firework->setPosition(this->getLocation());
+	firework->setPositionType(cocos2d::kCCPositionTypeRelative);
+
+	this->model->vista->clouds->addChild(firework);
+
+	for (int i = 0; i < this->model->enemies.size(); i++) {
+		Enemy* enemy = this->model->enemies[i];
+
+		if (MathUtil::distance(firework->getPosition(), enemy->getLocation())
+				< 200) {
+			enemy->hurt(30);
+		}
+	}
+}
 
 
 } /* namespace dxco */
