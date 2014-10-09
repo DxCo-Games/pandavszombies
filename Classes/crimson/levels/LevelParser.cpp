2cc674d0b4e2e0fac3cd8aa8269340e29e259a9f
gsosarolon <gonzalososarolon@gmail.com>
2014-10-09 19:30:41 -0300

    fix en parser mis amigos

--- Classes/crimson/levels/LevelParser.cpp
+++ Classes/crimson/levels/LevelParser.cpp
@@ -13,15 +13,14 @@ namespace dxco {
 std::vector<EnemyWave*> LevelParser::parse(GameModel* model, std::string levelPath) {
 
 	std::vector<EnemyWave*> resultado;
-	CCLOG("hola1");
 	rapidjson::Document* document = JsonParser::parseJsonFile(levelPath);
-	CCLOG("hola2");
+
 	for (rapidjson::SizeType i = 0; i < (*document).Size(); i++) {
 
 			const rapidjson::Value& waveConfig = (*document)[i];
-			CCLOG("hola3");
+
 			int total = waveConfig["total"].GetInt();
-			float freq = waveConfig["total"].GetDouble();
+			float freq = waveConfig["freq"].GetDouble();
 			float speed = waveConfig["speed"].GetDouble();
 			int isBoss = waveConfig["isBoss"].GetInt();
 
