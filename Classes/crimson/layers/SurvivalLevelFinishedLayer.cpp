61fe6b5ef053c6930233c25e7efb84fc5523fd25
leandro-romero <leandro.s.romero@gmail.com>
2014-10-23 19:56:33 -0300

    Fix para olano. Anchorpoint.

--- Classes/crimson/layers/SurvivalLevelFinishedLayer.cpp
+++ Classes/crimson/layers/SurvivalLevelFinishedLayer.cpp
@@ -35,38 +35,43 @@ SurvivalLevelFinishedLayer::SurvivalLevelFinishedLayer(float x, float y, float w
 	this->addChild(tryAgainButtonSprite);
 	this->tryAgainButton = new SurvivalButton(tryAgainButtonSprite);
 
-	this->pointsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::RIGHT, "fonts/KBStickToThePlan.ttf");
+	this->pointsLabel = cocos2d::CCLabelTTF::create("0", "fonts/KBStickToThePlan.ttf", 14);
+	pointsLabel->setAnchorPoint(ccp(1,0.5f));
 
 	pointsLabel->setPositionX(placaGameOver->getPositionX() - 0.26 * dxco::SpriteUtil::getWidth(placaGameOver));
-	pointsLabel->setPositionY(placaGameOver->getPositionY() - 0.20 * dxco::SpriteUtil::getHeight(placaGameOver));
+	pointsLabel->setPositionY(placaGameOver->getPositionY() - 0.1 * dxco::SpriteUtil::getHeight(placaGameOver));
 	this->addChild(pointsLabel, 10);
 
 	cocos2d::CCSprite* trophy = SpriteUtil::create("trophy.png", visibleSize.width * 0.2, visibleSize.height * 0.7, dxco::SpriteUtil::getWidth(placaGameOver) * 0.13, dxco::SpriteUtil::getHeight(placaGameOver) * 0.3);
 	trophy->setPositionX(pointsLabel->getPositionX() + 0.05 * dxco::SpriteUtil::getWidth(placaGameOver));
-	trophy->setPositionY(pointsLabel->getPositionY() + 0.08 * dxco::SpriteUtil::getHeight(placaGameOver));
+	trophy->setPositionY(pointsLabel->getPositionY());
 
 	this->addChild(trophy, 10);
 
-	this->killsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::RIGHT, "fonts/KBStickToThePlan.ttf");
+	this->killsLabel = cocos2d::CCLabelTTF::create("0", "fonts/KBStickToThePlan.ttf", 14);
+	killsLabel->setAnchorPoint(ccp(1,0.5f));
+
 	killsLabel->setPositionX(trophy->getPositionX() + 0.23 * dxco::SpriteUtil::getWidth(placaGameOver));
 	killsLabel->setPositionY(pointsLabel->getPositionY());
 
 	this->addChild(killsLabel, 10);
 
 	cocos2d::CCSprite* cross = SpriteUtil::create("cross.png", visibleSize.width * 0.2, visibleSize.height * 0.7, dxco::SpriteUtil::getWidth(placaGameOver) * 0.13, dxco::SpriteUtil::getHeight(placaGameOver) * 0.3);
-	cross->setPositionX(killsLabel->getPositionX() + 0.08 * dxco::SpriteUtil::getWidth(placaGameOver));
+	cross->setPositionX(killsLabel->getPositionX() + 0.05 * dxco::SpriteUtil::getWidth(placaGameOver));
 	cross->setPositionY(trophy->getPositionY());
 
 	this->addChild(cross, 10);
 
-	this->coinsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::RIGHT, "fonts/KBStickToThePlan.ttf");
-	coinsLabel->setPositionX(cross->getPositionX() + 0.17 * dxco::SpriteUtil::getWidth(placaGameOver));
+	this->coinsLabel = cocos2d::CCLabelTTF::create("0", "fonts/KBStickToThePlan.ttf", 14);
+	coinsLabel->setAnchorPoint(ccp(1,0.5f));
+
+	coinsLabel->setPositionX(cross->getPositionX() + 0.20 * dxco::SpriteUtil::getWidth(placaGameOver));
 	coinsLabel->setPositionY(pointsLabel->getPositionY());
 
 	this->addChild(coinsLabel, 10);
 
 	cocos2d::CCSprite* coin = SpriteUtil::create("coin.png", visibleSize.width * 0.2, visibleSize.height * 0.7, dxco::SpriteUtil::getWidth(placaGameOver) * 0.13, dxco::SpriteUtil::getHeight(placaGameOver) * 0.3);
-	coin->setPositionX(coinsLabel->getPositionX() + 0.08 * dxco::SpriteUtil::getWidth(placaGameOver));
+	coin->setPositionX(coinsLabel->getPositionX() + 0.05 * dxco::SpriteUtil::getWidth(placaGameOver));
 	coin->setPositionY(trophy->getPositionY());
 
 	this->addChild(coin, 10);
