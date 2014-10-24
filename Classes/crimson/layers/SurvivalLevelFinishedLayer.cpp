7370e00618a5b7b57c690a1a4e2aee0ffb951859
leandro-romero <leandro.s.romero@gmail.com>
2014-10-24 12:43:16 -0300

    Mejora survival con notacion k.

--- Classes/crimson/layers/SurvivalLevelFinishedLayer.cpp
+++ Classes/crimson/layers/SurvivalLevelFinishedLayer.cpp
@@ -35,38 +35,38 @@ SurvivalLevelFinishedLayer::SurvivalLevelFinishedLayer(float x, float y, float w
 	this->addChild(tryAgainButtonSprite);
 	this->tryAgainButton = new SurvivalButton(tryAgainButtonSprite);
 
-	this->pointsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::LEFT, "fonts/KBStickToThePlan.ttf");
+	this->pointsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::RIGHT, "fonts/KBStickToThePlan.ttf");
 
-	pointsLabel->setPositionX(placaGameOver->getPositionX() - 0.37 * dxco::SpriteUtil::getWidth(placaGameOver));
+	pointsLabel->setPositionX(placaGameOver->getPositionX() - 0.26 * dxco::SpriteUtil::getWidth(placaGameOver));
 	pointsLabel->setPositionY(placaGameOver->getPositionY() - 0.20 * dxco::SpriteUtil::getHeight(placaGameOver));
 	this->addChild(pointsLabel, 10);
 
 	cocos2d::CCSprite* trophy = SpriteUtil::create("trophy.png", visibleSize.width * 0.2, visibleSize.height * 0.7, dxco::SpriteUtil::getWidth(placaGameOver) * 0.13, dxco::SpriteUtil::getHeight(placaGameOver) * 0.3);
-	trophy->setPositionX(pointsLabel->getPositionX() + 0.11 * dxco::SpriteUtil::getWidth(placaGameOver));
+	trophy->setPositionX(pointsLabel->getPositionX() + 0.05 * dxco::SpriteUtil::getWidth(placaGameOver));
 	trophy->setPositionY(pointsLabel->getPositionY() + 0.08 * dxco::SpriteUtil::getHeight(placaGameOver));
 
 	this->addChild(trophy, 10);
 
-	this->killsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::LEFT, "fonts/KBStickToThePlan.ttf");
-	killsLabel->setPositionX(trophy->getPositionX() + 0.17 * dxco::SpriteUtil::getWidth(placaGameOver));
+	this->killsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::RIGHT, "fonts/KBStickToThePlan.ttf");
+	killsLabel->setPositionX(trophy->getPositionX() + 0.23 * dxco::SpriteUtil::getWidth(placaGameOver));
 	killsLabel->setPositionY(pointsLabel->getPositionY());
 
 	this->addChild(killsLabel, 10);
 
 	cocos2d::CCSprite* cross = SpriteUtil::create("cross.png", visibleSize.width * 0.2, visibleSize.height * 0.7, dxco::SpriteUtil::getWidth(placaGameOver) * 0.13, dxco::SpriteUtil::getHeight(placaGameOver) * 0.3);
-	cross->setPositionX(killsLabel->getPositionX() + 0.13 * dxco::SpriteUtil::getWidth(placaGameOver));
+	cross->setPositionX(killsLabel->getPositionX() + 0.08 * dxco::SpriteUtil::getWidth(placaGameOver));
 	cross->setPositionY(trophy->getPositionY());
 
 	this->addChild(cross, 10);
 
-	this->coinsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::LEFT, "fonts/KBStickToThePlan.ttf");
+	this->coinsLabel = dxco::LabelUtil::create(StringUtil::toString(0), 14, visibleSize.width / 2, 10, dxco::LabelUtil::BOTTOM, dxco::LabelUtil::RIGHT, "fonts/KBStickToThePlan.ttf");
 	coinsLabel->setPositionX(cross->getPositionX() + 0.17 * dxco::SpriteUtil::getWidth(placaGameOver));
 	coinsLabel->setPositionY(pointsLabel->getPositionY());
 
 	this->addChild(coinsLabel, 10);
 
 	cocos2d::CCSprite* coin = SpriteUtil::create("coin.png", visibleSize.width * 0.2, visibleSize.height * 0.7, dxco::SpriteUtil::getWidth(placaGameOver) * 0.13, dxco::SpriteUtil::getHeight(placaGameOver) * 0.3);
-	coin->setPositionX(coinsLabel->getPositionX() + 0.13 * dxco::SpriteUtil::getWidth(placaGameOver));
+	coin->setPositionX(coinsLabel->getPositionX() + 0.08 * dxco::SpriteUtil::getWidth(placaGameOver));
 	coin->setPositionY(trophy->getPositionY());
 
 	this->addChild(coin, 10);
@@ -77,9 +77,9 @@ SurvivalLevelFinishedLayer::SurvivalLevelFinishedLayer(float x, float y, float w
 
 void SurvivalLevelFinishedLayer::show(int points, int kills, int coins) {
 
-	this->killsLabel->setString(StringUtil::toString(kills).c_str());
-	this->coinsLabel->setString(StringUtil::toString(coins).c_str());
-	this->pointsLabel->setString(StringUtil::toString(points).c_str());
+	this->killsLabel->setString(StringUtil::intToKString(kills).c_str());
+	this->coinsLabel->setString(StringUtil::intToKString(coins).c_str());
+	this->pointsLabel->setString(StringUtil::intToKString(points).c_str());
 
 	this->setVisible(true);
 	this->setTouchEnabled(true);
