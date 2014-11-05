4819dd4e31151e4d5f9c0bfc81c7cff494721724
gsosarolon <gonzalososarolon@gmail.com>
2014-11-04 22:43:41 -0300

    Avances en el equip panda

--- Classes/crimson/layers/EquipPandaItem.cpp
+++ Classes/crimson/layers/EquipPandaItem.cpp
@@ -1,13 +1,17 @@
 #include "EquipPandaItem.h"
 #include "../../dxco/SpriteUtil.h"
 #include "../../dxco/LabelUtil.h"
+#include "../../dxco/StringUtil.h"
 #include "cocos2d.h"
+
+#include <cstdlib>
 
 namespace dxco {
 
 
 EquipPandaItem::EquipPandaItem(std::string item, float x, float y, float width, float height, cocos2d::CCSprite *master) : Container(x, y, width, height), Touchable(this) {
 	this->item = item;
+	this->cantidadDisponible = rand() % 2;
 
 	cocos2d::CCSprite* background = SpriteUtil::create(this->getImagePath().c_str(), 0, height * 0.2, width, height * 0.8);
 	SpriteUtil::copyScale(master, background);
@@ -48,23 +52,70 @@ EquipPandaItem::EquipPandaItem(std::string item, float x, float y, float width, 
 
 	this->addChild(coinsLabel);
 
+	std::string cantidadStr = StringUtil::toString(this->getCantidadDisponible());
+
+	cocos2d::CCLabelTTF* cantidadLabel = LabelUtil::create(cantidadStr, 18, 0, 0, 0, 0, "fonts/KBStickToThePlan.ttf");
+
+	cantidadLabel->setAnchorPoint(ccp(0.5, 0.5));
+	LabelUtil::setScaleByHeight(cantidadLabel, width * 0.15);
+
+	float cantidadLabelHeight = LabelUtil::getHeight(cantidadLabel);
+	float cantidadLabelWidth = LabelUtil::getWidth(cantidadLabel);
+
+	float cantidadLabelX = (cantidadLabelWidth / 2) + origin.x + width * 0.09;
+	float cantidadLabelY = height - (cantidadLabelHeight / 2) + origin.y - width * 0.072;
+
+	cantidadLabel->setPositionX(cantidadLabelX);
+	cantidadLabel->setPositionY(cantidadLabelY);
+
+	if (!this->isActivo()) {
+		cantidadLabel->setColor(cocos2d::ccc3(181, 181, 181));
+	}
+
+	this->addChild(cantidadLabel);
+
 	cocos2d::CCSprite* buyButton = SpriteUtil::create(this->getBuyImagePath(), 0, 0, width, height * 0.15);
 	SpriteUtil::copyScale(master, buyButton);
 	this->addChild(buyButton);
 }
 
+bool EquipPandaItem::isActivo() {
+	return this->cantidadDisponible > 0;
+}
+
 std::string EquipPandaItem::getBuyImagePath() {
-	return "buttons/buy.png";
+	if (this->isActivo()) {
+		return "buttons/buy.png";
+	}
+
+	return "buttons/buy-inactivo.png";
+}
+
+int EquipPandaItem::getCantidadDisponible() {
+	return this->cantidadDisponible;
 }
 
 std::string EquipPandaItem::getImagePath() {
 
 	if (item == "BOMB") {
-		return "equip/MEJORA_bomba_act.png";
+
+		if (this->isActivo()) {
+			return "equip/MEJORA_bomba_act.png";
+		} else {
+			return "equip/MEJORA_bomba_desac.png";
+		}
 	} else if (item == "SHOT") {
-		return "equip/MEJORA_balas_act.png";
+		if (this->isActivo()) {
+			return "equip/MEJORA_balas_act.png";
+		} else {
+			return "equip/MEJORA_balas_desac.png";
+		}
 	} else {
-		return "equip/MEJORA_bonus_act.png";
+		if (this->isActivo()) {
+			return "equip/MEJORA_bonus_act.png";
+		} else {
+			return "equip/MEJORA_bonus_desac.png";
+		}
 	}
 }
 
@@ -82,6 +133,9 @@ std::string EquipPandaItem::_getPrice() {
 
 void EquipPandaItem::execute() {
 
+	if (this->isActivo()) {
+		// TODO equip
+	}
 }
 
 } /* namespace dxco */
